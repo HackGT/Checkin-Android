a6148590fc8d326061d8e7c181a7223a695aa5a4
Evan Strat <evan10s@users.noreply.github.com>
2018-09-29 23:56:42 -0400

    Re-implemented a bunch of the NFC functionality

--- app/src/main/java/gt/hack/nfc/fragment/TapFragment.kt
+++ app/src/main/java/gt/hack/nfc/fragment/TapFragment.kt
@@ -6,9 +6,12 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.DialogInterface.OnClickListener
 import android.graphics.Color
+import android.net.Uri
+import android.nfc.NdefRecord
 import android.nfc.tech.Ndef
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import android.nfc.tech.NfcA
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -16,10 +19,12 @@ import android.os.Handler
 import android.preference.PreferenceManager
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.apollographql.apollo.exception.ApolloNetworkException
 import gt.hack.nfc.R
 import gt.hack.nfc.util.API
 import gt.hack.nfc.util.Util
@@ -29,39 +34,128 @@ import kotlinx.android.synthetic.main.fragment_tap.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.coroutines.experimental.runBlocking
+import java.lang.RuntimeException
+import kotlin.collections.ArrayList
 
 
 class TapFragment : Fragment() {
 
   val READER_FLAGS = NfcAdapter.FLAG_READER_NFC_A
+  val TAG = "CHECKIN/TAP_FRAGMENT"
+
+  companion object {
+    fun newInstance(): TapFragment {
+      return TapFragment()
+    }
+  }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    inflater.inflate(R.layout.fragment_tap, container, false)
-    return super.onCreateView(inflater, container, savedInstanceState)
+    return inflater.inflate(R.layout.fragment_tap, container, false)
+    //return super.onCreateView(inflater, container, savedInstanceState)
+  }
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    val uuidRegex = Regex("[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}")
+    super.onViewCreated(view, savedInstanceState)
+    Log.i("checkin-tap", "inside onviewcreated")
+    val tagSelect: AutoCompleteTextView = checkin_tag
+    val prevTag = tagSelect.text
+    tagSelect.setText("Updating tags list...")
+    tagSelect.isEnabled = false
+
+    val preferences = PreferenceManager.getDefaultSharedPreferences(getActivity())
+    var error = false;
+    val allTags = runBlocking {
+      try {
+        API.getTags(preferences)
+      } catch (e: ApolloNetworkException) { // Working way to detect errors inside the network
+        // operation call and then make the UI do something in response
+        error = true
+      }
+    } // TODO: this seems to delay showing this screen, so check that out
+    Log.i(TAG, "Contents of allTags: " + allTags)
+
+    if (error) {
+      tagSelect.setText("No internet, couldn't fetch tags")
+    } else {
+      tagSelect.setText(prevTag)
+      tagSelect.isEnabled = true
+
+      val autocomplete = ArrayAdapter(context, android.R.layout.simple_expandable_list_item_1, allTags as ArrayList<String>)
+      // the cast to ArrayList is kinda icky but so long as the API response is OK, it should work
+      tagSelect.threshold = 0
+      tagSelect.setAdapter(autocomplete)
+      tagSelect.setOnFocusChangeListener { v, hasFocus ->
+        if (!hasFocus) {
+          Util.hideSoftKeyboard(view, context)
+        }
+      }
+
+      check_in_out_select.setOnCheckedChangeListener { buttonView, isChecked ->
+        check_in_out_select.text = when (isChecked) {
+          true -> getString(R.string.switch_check_in)
+          false -> getString(R.string.switch_check_out)
+        }
+      }
+
+      val nfc = NfcAdapter.getDefaultAdapter(activity)
+      nfc.enableReaderMode(activity, { tag: Tag ->
+        // get the latest read tag
+        val ndef = Ndef.get(tag)
+        val record: NdefRecord? = try {
+          ndef.connect()
+          val message = ndef.ndefMessage
+          message.records[0]
+        } catch (e: Throwable) {
+          Log.i(TAG,"" + e.printStackTrace())
+          null
+        } finally {
+          try {
+            ndef.close()
+          } catch (e: Throwable) {
+            e.printStackTrace()
+          }
+        }
+
+        val id: Uri? = record?.toUri()
+        Log.i(TAG, id.toString())
+        if (id?.host == "live.hack.gt"  && uuidRegex.containsMatchIn(id.getQueryParameter("user").orEmpty())) {
+          val uuid = id.getQueryParameter("user").orEmpty()
+          Log.i(TAG, "this valid id is " + uuid)
+
+          val tagName = tagSelect.text.trim().toString()
+          val doCheckIn = check_in_out_select.isChecked
+            // check in/out the user
+            // TODO: extract this to a function
+            val userInfo = runBlocking { API.getUserById(preferences, uuid) }
+            val currentTags = runBlocking { API.getTagsForUser(preferences, uuid) }
+            val newTags = when (doCheckIn) {
+              true -> runBlocking { API.checkInTag(preferences, uuid, tagName) }
+              else -> runBlocking { API.checkOutTag(preferences, uuid, tagName) }
+            }
+
+            val checkInData = CheckInData(userInfo, currentTags, newTags!!)
+
+
+          Log.i(TAG, checkInData.userInfo.toString())
+          Log.i(TAG, checkInData.currentTags.toString())
+          Log.i(TAG, checkInData.newTags.toString())
+        } else {
+          Log.i(TAG, "this tag's data is formatted incorrectly: " + id)
+          //invalidTagMessage(), probs a toast
+        }
+
+
+
+      }, READER_FLAGS, null)
+
+    }
+    Util.makeSnackbar(activity?.findViewById(R.id.content_frame), R.string.tags_api_made_past, Snackbar.LENGTH_SHORT).show()
   }
 
   override fun onResume() {
     super.onResume()
-    val preferences = PreferenceManager.getDefaultSharedPreferences(getActivity())
-    //val uuidRegex = "[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}".r
-
-    //
-    // Fetch Latest Tags
-    //
-    // get the text view, save the last text and notify we are updating the tags list
-    val tagSelect: AutoCompleteTextView = activity!!.findViewById(R.id.checkin_tag)
-    val prevTag = tagSelect.getText()
-    tagSelect.setText("Updating tags list...")
-    tagSelect.setEnabled(false)
-
-    // asynchronously get the latest tags
-
-    val allTags = runBlocking {
-      API.getTags(preferences)
-    }
-
-      Util.makeSnackbar(activity?.findViewById(R.id.content_frame), R.string.tags_api_made_past, Snackbar.LENGTH_SHORT).show()
+    Log.i(TAG, "Inside onResume")
 //
 //      activity?.runOnUiThread(Runnable {
 //          tagSelect.setText(prevTag)
@@ -234,34 +328,34 @@ class TapFragment : Fragment() {
 
   fun invalidTagMessage() {
     Util.makeSnackbar(
-      activity?.content_frame,
-      R.string.invalid_nfc_tag,
-      Snackbar.LENGTH_SHORT
+        activity?.content_frame,
+        R.string.invalid_nfc_tag,
+        Snackbar.LENGTH_SHORT
     ).show()
   }
 
   fun showAlert(title: String, message: Int) {
-      val builder : AlertDialog.Builder
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-          builder = AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert)
-      } else {
-          builder = AlertDialog.Builder(getContext())
-      }
+    val builder: AlertDialog.Builder
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      builder = AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert)
+    } else {
+      builder = AlertDialog.Builder(getContext())
+    }
 
 
     builder.setTitle(title)
-      .setMessage(message)
-      .setNeutralButton(android.R.string.ok, OnClickListener({ dialog, which ->  } ))
-      .setIcon(android.R.drawable.ic_dialog_alert)
-      .show()
+        .setMessage(message)
+        .setNeutralButton(android.R.string.ok, OnClickListener({ dialog, which -> }))
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .show()
   }
 
   data class CheckInData(val userInfo: UserFragment?, val currentTags: HashMap<String, TagFragment>?, val newTags: HashMap<String, TagFragment>)
 }
 
-  //
-  // Important Utilities!
-  //
+//
+// Important Utilities!
+//
 //  fun runOnUi[F](fn: () => F): Future[F] = {
 //    val complete = Promise[F]()
 //    getActivity.runOnUiThread(() => {
