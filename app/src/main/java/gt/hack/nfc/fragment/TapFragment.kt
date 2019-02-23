4fcf5bda8d5f4adb6126c07caa038ee860fa04b5
Evan Strat <evan10s@users.noreply.github.com>
2019-02-22 22:29:51 -0500

    Display error on invalid uuid scanned
    Formatted 2 spaces, start work on showing date/user for invalid check-ins

--- app/src/main/java/gt/hack/nfc/fragment/TapFragment.kt
+++ app/src/main/java/gt/hack/nfc/fragment/TapFragment.kt
@@ -11,6 +11,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
+
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -26,237 +27,242 @@ import kotlinx.coroutines.runBlocking
 
 class TapFragment : Fragment() {
 
-    val TAG = "CHECKIN/TAP_FRAGMENT"
-    var waitingForTag = true
-    private var nfcHandler = NFCHandler()
+  val TAG = "CHECKIN/TAP_FRAGMENT"
+  var waitingForTag = true
+  private var nfcHandler = NFCHandler()
 
-    companion object {
-        fun newInstance(): TapFragment {
-            return TapFragment()
-        }
+  companion object {
+    fun newInstance(): TapFragment {
+      return TapFragment()
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_tap, container, false)
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    // in case NFC becomes enabled in settings
+    loadNFC()
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    Log.i("checkin-tap", "inside onviewcreated")
+    val tagSelect: AutoCompleteTextView = checkin_tag
+    val prevTag = tagSelect.text
+    tagSelect.setText("Updating tags list...")
+    tagSelect.isEnabled = false
+
+    val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+    var allTags = runBlocking { API.getTags(preferences) } // TODO: this seems to delay showing this screen, so check that out
+    val currentCheckinTags = runBlocking { API.getTags(preferences, true) }
+
+    allTags = allTags as ArrayList<String>
+    allTags.sortWith(kotlin.Comparator { t1, t2 ->
+      val t1SortVal = when (currentCheckinTags?.contains(t1)) {
+        true -> -1
+        else -> 0
+      }
+      val t2SortVal = when (currentCheckinTags?.contains(t2)) {
+        true -> -1
+        else -> 0
+      }
+      t1SortVal - t2SortVal
+    })
+    Log.i(TAG, "Contents of sorted allTags: " + allTags)
+
+    tagSelect.text = prevTag
+    tagSelect.isEnabled = true
+
+    val autocomplete = ArrayAdapter(context, android.R.layout.simple_expandable_list_item_1, allTags)
+    // the cast to ArrayList is kinda icky but so long as the API response is OK, it should work
+    tagSelect.threshold = 1
+    tagSelect.setAdapter(autocomplete)
+    tagSelect.setOnFocusChangeListener { v, hasFocus ->
+      if (!hasFocus) {
+        Util.hideSoftKeyboard(view, context!!)
+      } else {
+        tagSelect.showDropDown() // make sure the suggestions show even if no characters have been entered
+      }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_tap, container, false)
+    check_in_out_select.setOnCheckedChangeListener { _, isChecked ->
+      check_in_out_select.text = when (isChecked) {
+        true -> getString(R.string.switch_check_in)
+        false -> getString(R.string.switch_check_out)
+      }
     }
 
-    override fun onResume() {
-        super.onResume()
+    nfcHandler.activity = activity
+    nfcHandler.context = context
+    nfcHandler.callback = NfcAdapter.ReaderCallback { tag: Tag ->
+      val uuidRegex = Regex("[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}")
+      val tagSelect: AutoCompleteTextView = checkin_tag
+      val preferences = PreferenceManager.getDefaultSharedPreferences(activity)
 
-        // in case NFC becomes enabled in settings
-        loadNFC()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.i("checkin-tap", "inside onviewcreated")
-        val tagSelect: AutoCompleteTextView = checkin_tag
-        val prevTag = tagSelect.text
-        tagSelect.setText("Updating tags list...")
-        tagSelect.isEnabled = false
-
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        var allTags = runBlocking { API.getTags(preferences) } // TODO: this seems to delay showing this screen, so check that out
-        val currentCheckinTags = runBlocking { API.getTags(preferences, true) }
-
-        allTags = allTags as ArrayList<String>
-        allTags.sortWith(kotlin.Comparator { t1, t2 ->
-            val t1SortVal = when (currentCheckinTags?.contains(t1)) {
-                true -> -1
-                else -> 0
-            }
-            val t2SortVal = when (currentCheckinTags?.contains(t2)) {
-                true -> -1
-                else -> 0
-            }
-            t1SortVal - t2SortVal
-        })
-        Log.i(TAG, "Contents of sorted allTags: " + allTags)
-
-        tagSelect.text = prevTag
-        tagSelect.isEnabled = true
-
-        val autocomplete = ArrayAdapter(context, android.R.layout.simple_expandable_list_item_1, allTags)
-        // the cast to ArrayList is kinda icky but so long as the API response is OK, it should work
-        tagSelect.threshold = 1
-        tagSelect.setAdapter(autocomplete)
-        tagSelect.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {
-                Util.hideSoftKeyboard(view, context!!)
-            } else {
-                tagSelect.showDropDown() // make sure the suggestions show even if no characters have been entered
-            }
-        }
-
-        check_in_out_select.setOnCheckedChangeListener { _, isChecked ->
-            check_in_out_select.text = when (isChecked) {
-                true -> getString(R.string.switch_check_in)
-                false -> getString(R.string.switch_check_out)
-            }
-        }
-
-        nfcHandler.activity = activity
-        nfcHandler.context = context
-        nfcHandler.callback = NfcAdapter.ReaderCallback { tag: Tag ->
-            val uuidRegex = Regex("[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}")
-            val tagSelect: AutoCompleteTextView = checkin_tag
-            val preferences = PreferenceManager.getDefaultSharedPreferences(activity)
-
-            if (waitingForTag) {
-                waitingForTag = false
-                // get the latest read tag
-                val ndef = Ndef.get(tag)
-                val record: NdefRecord? = try {
-                    ndef.connect()
-                    val message = ndef.ndefMessage
-                    message.records[0]
-                } catch (e: Throwable) {
-                    Log.i(TAG, "" + e.printStackTrace())
-                    null
-                } finally {
-                    try {
-                        ndef.close()
-                    } catch (e: Throwable) {
-                        e.printStackTrace()
-                    }
-                }
-
-                val id: Uri? = record?.toUri()
-                Log.i(TAG, id.toString())
-                if (id?.host == "live.hack.gt" && uuidRegex.containsMatchIn(id.getQueryParameter("user").orEmpty())) {
-                    val uuid = id.getQueryParameter("user").orEmpty()
-                    Log.i(TAG, "this valid id is " + uuid)
-
-                    val tagName = tagSelect.text.trim().toString()
-                    val doCheckIn = check_in_out_select.isChecked
-
-                    val checkinResult = runBlocking { API.checkInTag(preferences, uuid, tagName, doCheckIn) };
-
-                    if (checkinResult != null) {
-                        var checkinDetails : TagFragment? = null;
-                        val checkinTagInfo = checkinResult.tags().findLast { it.fragments().tagFragment().tag.name.equals(tagName) }
-                        if (checkinTagInfo != null) {
-                            checkinDetails = checkinTagInfo.fragments().tagFragment();
-                        }
-
-                        val userInfo = checkinResult.user().fragments().userFragment()
-
-                        val checkInData = CheckInData(userInfo, checkinDetails);
-
-                        drawCheckInFinish(checkInData);
-                    }
-                } else {
-                    if (id == null) {
-                        Log.i(TAG, "this tag's data is null: " + id)
-                        displayMessageAndReset(false, getString(R.string.badge_data_null), 2500)
-                    } else {
-                        Log.i(TAG, "this tag's data is formatted incorrectly: " + id)
-                        displayMessageAndReset(false, getString(R.string.invalid_badge_id), 4000)
-                    }
-                }
-
-            }
-
+      if (waitingForTag) {
+        waitingForTag = false
+        // get the latest read tag
+        val ndef = Ndef.get(tag)
+        val record: NdefRecord? = try {
+          ndef.connect()
+          val message = ndef.ndefMessage
+          message.records[0]
+        } catch (e: Throwable) {
+          Log.i(TAG, "" + e.printStackTrace())
+          null
+        } finally {
+          try {
+            ndef.close()
+          } catch (e: Throwable) {
+            e.printStackTrace()
+          }
         }
 
-        loadNFC()
-    }
+        val id: Uri? = record?.toUri()
+        Log.i(TAG, id.toString())
+        if (id?.host == "live.hack.gt" && uuidRegex.containsMatchIn(id.getQueryParameter("user").orEmpty())) {
+          val uuid = id.getQueryParameter("user").orEmpty()
+          Log.i(TAG, "this valid id is " + uuid)
 
-    fun loadNFC() {
-        nfcHandler.loadNFC(nfcInstructions, wait_for_badge_tap, nfc_error, enable_nfc_button)
-    }
+          var tagName = tagSelect.text.trim().toString()
+          tagName = tagName.toLowerCase()
+          val doCheckIn = check_in_out_select.isChecked
 
-    fun drawCheckInFinish(checkInData: CheckInData) {
-        val waitingForBadge = wait_for_badge_tap
-        val userName = track_name
-        val userBranch = track_type
-        val userShirtSize = track_tshirt_size
-        val userDietaryRestrictions = track_dietary_restrictions
+          val checkinResult = runBlocking { API.checkInTag(preferences, uuid, tagName, doCheckIn) }
 
-        val userInfo = checkInData.userInfo
-        var userShirtSizeVal: String? = ""
-        var userDietaryRestrictionsVal: String? = ""
-        if (userInfo != null && checkInData.checkInResult != null) {
-
-            userInfo.questions.forEach { question: UserFragment.Question? ->
-                if (question?.name.equals("tshirt-size")) {
-                    userShirtSizeVal = question?.value
-                } else if (question?.name.equals("dietary-restrictions")) {
-                    userDietaryRestrictionsVal = question?.values.toString()
-                }
+          if (checkinResult != null) {
+            var checkinDetails: TagFragment? = null
+            val checkinTagInfo = checkinResult.tags().findLast { it.fragments().tagFragment().tag.name.equals(tagName) }
+            if (checkinTagInfo != null) {
+              checkinDetails = checkinTagInfo.fragments().tagFragment()
             }
 
+            val userInfo = checkinResult.user().fragments().userFragment()
 
-            activity?.runOnUiThread {
-                userName.text = userInfo.name
-                if (userInfo.application != null) {
-                    userBranch.text = userInfo.application.type
-                }
+            val checkInData = CheckInData(userInfo, checkinDetails)
 
-                userShirtSize.text = userShirtSizeVal
-                try {
-                    userDietaryRestrictions.text = userDietaryRestrictionsVal?.substring(1)?.dropLast(1)
-                } catch (e: StringIndexOutOfBoundsException) {
-                    userDietaryRestrictions.text = ""
-                }
-
-                waitingForBadge.visibility = View.GONE
-                nfcInstructions.visibility = View.GONE
-            }
-
-            val validOperation: Boolean = checkInData.checkInResult.checkin_success
-
-            if (validOperation) {
-                displayMessageAndReset(true, "", 750)
-            } else if (checkInData.checkInResult.checked_in) { // indicates checkin/out state
-                displayMessageAndReset(false, getString(R.string.user_already_checked_in), 3000)
-                Log.i(TAG, checkInData.checkInResult.last_successful_checkin()?.checked_in_date);
-                Log.i(TAG, checkInData.checkInResult.last_successful_checkin()?.checked_in_by);
-            } else if (!checkInData.checkInResult.checked_in) {
-                //displayMessageAndReset(false, getString(R.string.cannot_checkout_not_checked_in), 5000)
-                displayMessageAndReset(false, getString(R.string.user_already_checked_out), 3000)
-            } else {
-                displayMessageAndReset(false, "Edge case encountered ... somehow", 3000)
-            }
-
-
-        } else { // checkInData is null, ie invalid user
+            drawCheckInFinish(checkInData)
+          } else {
+            Log.i(TAG, "checkin API returned null")
+            displayMessageAndReset(false, "User ID or tag name not found", 2500)
+          }
+        } else {
+          if (id == null) {
+            Log.i(TAG, "this tag's data is null: " + id)
+            displayMessageAndReset(false, getString(R.string.badge_data_null), 2500)
+          } else {
+            Log.i(TAG, "this tag's data is formatted incorrectly: " + id)
             displayMessageAndReset(false, getString(R.string.invalid_badge_id), 4000)
+          }
         }
+
+      }
+
     }
 
-    fun displayMessageAndReset(validTag: Boolean, message: String, duration: Long) {
-        activity?.runOnUiThread {
-            val waitingForBadge = wait_for_badge_tap
-            val badgeTapped = badge_tapped
+    loadNFC()
+  }
 
-            waitingForBadge.visibility = View.GONE
-            nfcInstructions.visibility = View.GONE
+  fun loadNFC() {
+    nfcHandler.loadNFC(nfcInstructions, wait_for_badge_tap, nfc_error, enable_nfc_button)
+  }
 
-            if (validTag) {
-                badgeTapped.visibility = View.VISIBLE
-            } else {
-                val toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
-                toneGen1.startTone(ToneGenerator.TONE_CDMA_EMERGENCY_RINGBACK, 500)
+  fun drawCheckInFinish(checkInData: CheckInData) {
+    val waitingForBadge = wait_for_badge_tap
+    val userName = track_name
+    val userBranch = track_type
+    val userShirtSize = track_tshirt_size
+    val userDietaryRestrictions = track_dietary_restrictions
 
-                invalid_tap_msg.text = message
-                invalid_tap.visibility = View.VISIBLE
-                invalid_tap_msg.visibility = View.VISIBLE
-            }
+    val userInfo = checkInData.userInfo
+    var userShirtSizeVal: String? = ""
+    var userDietaryRestrictionsVal: String? = ""
+    if (userInfo != null && checkInData.checkInResult != null) {
 
-            Handler().postDelayed({
-                waitingForBadge.visibility = View.VISIBLE
-                nfcInstructions.visibility = View.VISIBLE
-
-                badgeTapped.visibility = View.GONE
-                invalid_tap.visibility = View.GONE
-                invalid_tap_msg.visibility = View.GONE
-
-                waitingForTag = true
-            }, duration)
+      userInfo.questions.forEach { question: UserFragment.Question? ->
+        if (question?.name.equals("tshirt-size")) {
+          userShirtSizeVal = question?.value
+        } else if (question?.name.equals("dietary-restrictions")) {
+          userDietaryRestrictionsVal = question?.values.toString()
         }
+      }
+
+
+      activity?.runOnUiThread {
+        userName.text = userInfo.name
+        if (userInfo.application != null) {
+          userBranch.text = userInfo.application.type
+        }
+
+        userShirtSize.text = userShirtSizeVal
+        try {
+          userDietaryRestrictions.text = userDietaryRestrictionsVal?.substring(1)?.dropLast(1)
+        } catch (e: StringIndexOutOfBoundsException) {
+          userDietaryRestrictions.text = ""
+        }
+
+        waitingForBadge.visibility = View.GONE
+        nfcInstructions.visibility = View.GONE
+      }
+
+      val validOperation: Boolean = checkInData.checkInResult.checkin_success
+
+      if (validOperation) {
+        displayMessageAndReset(true, "", 750)
+      } else if (checkInData.checkInResult.checked_in) { // indicates checkin/out state
+        displayMessageAndReset(false, getString(R.string.user_already_checked_in), 3000)
+        Log.i(TAG, checkInData.checkInResult.last_successful_checkin()?.checked_in_date)
+        //Log.i(TAG, DateUtils.getRelativeTimeSpanString((Date(checkInData.checkInResult.last_successful_checkin()?.checked_in_date)).toString())
+        Log.i(TAG, checkInData.checkInResult.last_successful_checkin()?.checked_in_by)
+      } else if (!checkInData.checkInResult.checked_in) {
+        //displayMessageAndReset(false, getString(R.string.cannot_checkout_not_checked_in), 5000)
+        displayMessageAndReset(false, getString(R.string.user_already_checked_out), 3000)
+      } else {
+        displayMessageAndReset(false, "Edge case encountered ... somehow", 3000)
+      }
+
+
+    } else { // checkInData is null, ie invalid user
+      displayMessageAndReset(false, getString(R.string.invalid_badge_id), 4000)
     }
+  }
+
+  fun displayMessageAndReset(validTag: Boolean, message: String, duration: Long) {
+    activity?.runOnUiThread {
+      val waitingForBadge = wait_for_badge_tap
+      val badgeTapped = badge_tapped
+
+      waitingForBadge.visibility = View.GONE
+      nfcInstructions.visibility = View.GONE
+
+      if (validTag) {
+        badgeTapped.visibility = View.VISIBLE
+      } else {
+        val toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_EMERGENCY_RINGBACK, 500)
+
+        invalid_tap_msg.text = message
+        invalid_tap.visibility = View.VISIBLE
+        invalid_tap_msg.visibility = View.VISIBLE
+      }
+
+      Handler().postDelayed({
+        waitingForBadge.visibility = View.VISIBLE
+        nfcInstructions.visibility = View.VISIBLE
+
+        badgeTapped.visibility = View.GONE
+        invalid_tap.visibility = View.GONE
+        invalid_tap_msg.visibility = View.GONE
+
+        waitingForTag = true
+      }, duration)
+    }
+  }
 
 
-    data class CheckInData(val userInfo: UserFragment?, val checkInResult: TagFragment?)
+  data class CheckInData(val userInfo: UserFragment?, val checkInResult: TagFragment?)
 }
